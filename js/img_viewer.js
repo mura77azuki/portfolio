505a188af95b3f940ce7c82d6d72756a1a2d5f5f
mura77azuki <murakami80557877@gmail.com>
2023-04-22 22:57:55 +0900

    画像が1枚でも正しく表示されるようにスクリプトを修正。ローディングアニメーションを追加。

--- js/img_viewer.js
+++ js/img_viewer.js
@@ -1,5 +1,6 @@
 class ImgViewer{
 	list = [];
+	img_num = 0;
 	container = undefined;
 	img = undefined;
 	btn_prev = undefined;
@@ -13,62 +14,95 @@ class ImgViewer{
 	 */
 	constructor(id, imglist){
 		this.list = imglist;
+		this.img_num = this.list.length;
+		//ビューを追加する要素を取得
 		this.container = document.getElementById(id);
 		this.container.classList.add('img_viewer');
-		
+		if(this.img_num < 1){
+			this.container.innerText = 'No Image';
+			return;
+		}
+		//ビューの中身を作るdiv要素を生成
 		this.content = document.createElement('div');
 		this.content.classList.add('viewer_body');
-
+		//img要素を生成して画像をセット
+		this.img = document.createElement('img');
+		this.img.addEventListener('load', ()=>{this.loadImg();});
+		this.img.addEventListener('error', ()=>{this.errorImg();});
+		this.setImg();
+		//画像が1枚だけの時はimg要素だけ追加して終了
+		if(this.img_num == 1){
+			this.content.appendChild(this.img);
+			this.container.appendChild(this.content);
+			return;
+		}
+		//prevボタンを生成&クリックイベント追加
 		this.btn_prev = document.createElement('div');
 		this.btn_prev.classList.add('btn_prev');
 		this.btn_prev.appendChild(document.createElement('span'));
-		this.btn_prev.addEventListener('click', ()=>{this.prev()});
-
-		this.img = document.createElement('img');
-		this.setImg();
-
+		this.btn_prev.addEventListener('click', ()=>{this.prev();});
+		//nextボタンを生成&クリックイベント追加
 		this.btn_next = document.createElement('div');
 		this.btn_next.classList.add('btn_next');
 		this.btn_next.appendChild(document.createElement('span'));
-		this.btn_next.addEventListener('click', ()=>{this.next()});
-
+		this.btn_next.addEventListener('click', ()=>{this.next();});
+		//要素をdivに追加
 		this.content.appendChild(this.btn_prev);
 		this.content.appendChild(this.img);
 		this.content.appendChild(this.btn_next);
-
+		//何枚目か表示する丸の部分を作る
 		this.position_bar = document.createElement('div');
 		this.position_bar.classList.add('position');
-		for(let i = 0; i < this.list.length; i++){
+		for(let i = 0; i < this.img_num; i++){
 			this.position_bar.appendChild(document.createElement('span'));
 		}
 		this.setPos();
-
+		//指定された要素に生成した中身を追加
 		this.container.appendChild(this.content);
 		this.container.appendChild(this.position_bar);
 	}
+	/**
+	 * 次の画像を表示する
+	 */
 	next(){
 		this.setPos();
 		this.index++;
-		if(this.index > this.list.length - 1){
+		if(this.index > this.img_num - 1){
 			this.index = 0;
 		}
 		this.setImg();
 		this.setPos();
 	}
+	/**
+	 * 前の画像を表示する
+	 */
 	prev(){
 		this.setPos();
 		this.index--;
 		if(this.index < 0){
-			this.index = this.list.length - 1;
+			this.index = this.img_num - 1;
 		}
 		this.setImg();
 		this.setPos();
 	}
+	/**
+	 * [index]番目の画像をセットする
+	 */
 	setImg(){
+		this.img.classList.add('loading');
 		this.img.src = this.list[this.index];
 	}
+	/**
+	 * [index]番目の丸のクラスリストで'active'をトグルする。
+	 */
 	setPos(){
 		this.position_bar.childNodes[this.index].classList.toggle('active');
+	}
+	loadImg(){
+		this.img.classList.remove('loading');
+	}
+	errorImg(){
+		this.img.classList.remove('loading');
 	}
 }
 
@@ -98,18 +132,15 @@ window.addEventListener('load',()=>{
 		'./img/work02/02.jpg',
 		'./img/work02/03.jpg',
 	]);
-
 	img_view4 = new ImgViewer('imgView4',[
 		'./img/work04/01.png',
 		'./img/work04/02.png',
 		'./img/work04/03.png',
 		'./img/work04/04.png',
 	]);
-	/*
 	img_view5 = new ImgViewer('imgView5',[
 		'./img/work05/01.png',
 	]);
-	*/
 	img_view6 = new ImgViewer('imgView6',[
 		'./img/work06/01.png',
 		'./img/work06/02.png',
